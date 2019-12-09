5b11db97596d6bba084eeff92e5671e6b87cd1e7
Gaurav Sharma <gauravsharmags06@gmail.com>
2019-12-09 18:53:50 +0530

    Updates

---
+++ electron.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const url = require("url");
+
+let mainWindow = null;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 768,
+    height: 560,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  const startUrl =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : url.format({
+          pathName: path.join(__dirname, "../build/index.html"),
+          slashes: true,
+          protocol: "file:"
+        });
+  mainWindow.loadURL(startUrl);
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
